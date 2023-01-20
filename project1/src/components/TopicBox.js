d672cf370b0d26d866ff7a5db8b8b8d4d721f2ad
Hasaranga <rvndhasaranga@gmail.com>
2023-01-21 03:46:50 +0530

    one component more data
    using props in parameters , and and add different details

--- project1/src/components/TopicBox.js
+++ project1/src/components/TopicBox.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import './topicbox.css'
 
-export default function TopicBox() {
+export default function TopicBox(props) {
   const topic = "My favourite food is kottu"
   const item = "and rice"
   const a = 20;
   const b = 30;
   const c = a+b;
+ 
   return (
     <div>
       <div className="topicbox">
-        <span className='text'>{topic} {item} {c}</span>
+        <span className='text'>{topic} {item} {c} {props.food} and price is {props.price} </span>
       </div>
     </div>
   )
--- project1/src/pages/Home.js
+++ project1/src/pages/Home.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import Content from '../components/Content'
+//import Content from '../components/Content'
 import TopicBox from '../components/TopicBox'
 
 export default function Home() {
   return (
     <div>
         <h1>Homepage</h1>
-        <Content/>
-      <TopicBox/>
+       <TopicBox food="kottu" price="250"/>
+       <TopicBox food="rice"price="350"/>
+       <TopicBox food="milk_rice"price="550"/>
+       
+
       
     </div>
   )
