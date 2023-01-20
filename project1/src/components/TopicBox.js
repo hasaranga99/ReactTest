766e0603b00e599680cbc4aeaf75d90564b478a0
Hasaranga <rvndhasaranga@gmail.com>
2023-01-21 03:22:22 +0530

    dianomic components
    javascript added before return code

--- project1/src/components/TopicBox.js
+++ project1/src/components/TopicBox.js
@@ -2,10 +2,15 @@ import React from 'react'
 import './topicbox.css'
 
 export default function TopicBox() {
+  const topic = "My favourite food is kottu"
+  const item = "and rice"
+  const a = 20;
+  const b = 30;
+  const c = a+b;
   return (
     <div>
       <div className="topicbox">
-        <span className='text'>This is a topic</span>
+        <span className='text'>{topic} {item} {c}</span>
       </div>
     </div>
   )
